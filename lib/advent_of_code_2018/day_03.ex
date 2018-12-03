632b8cad536c1c22620e8ef1469a700e9c60a70d
Kerry Buckley <kerryjbuckley@gmail.com>
2018-12-03 21:27:29 +0000

    Day 3 part 1

--- lib/advent_of_code_2018/day_03.ex
+++ lib/advent_of_code_2018/day_03.ex
@@ -1,5 +1,18 @@
 defmodule AdventOfCode2018.Day03 do
-  def part1(_args) do
+  def part1(args) do
+    ~r/^.*@ (\d+),(\d+): (\d+)x(\d+)/m
+    |> Regex.scan(args, capture: :all_but_first)
+    |> Enum.map(fn captures -> Enum.map(captures, &String.to_integer/1) end)
+    |> Enum.reduce(%{}, &record_covered/2)
+    |> Enum.count(fn {_, count} -> count > 1 end)
+  end
+
+  defp record_covered([x_origin, y_origin, dx, dy], covered) do
+    for x <- x_origin..(x_origin + dx - 1),
+        y <- y_origin..(y_origin + dy - 1) do
+      {x, y}
+    end
+    |> Enum.reduce(covered, fn coords, covered -> Map.update(covered, coords, 1, &(&1 + 1)) end)
   end
 
   def part2(_args) do
--- test/advent_of_code_2018/day_03_test.exs
+++ test/advent_of_code_2018/day_03_test.exs
@@ -3,17 +3,21 @@ defmodule AdventOfCode2018.Day03Test do
 
   import AdventOfCode2018.Day03
 
-  @tag :skip
   test "part1" do
-    input = nil 
+    input = """
+    #1 @ 1,3: 4x4
+    #2 @ 3,1: 4x4
+    #3 @ 5,5: 2x2
+    """
+
     result = part1(input)
 
-    assert result
+    assert result == 4
   end
 
   @tag :skip
   test "part2" do
-    input = nil 
+    input = nil
     result = part2(input)
 
     assert result
