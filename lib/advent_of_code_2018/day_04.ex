1a0fb0bbf6f1ab309f63b36fc00e308f3c4dcbb8
Kerry Buckley <kerryjbuckley@gmail.com>
2018-12-05 18:40:56 +0000

    Day 4 part 1

--- lib/advent_of_code_2018/day_04.ex
+++ lib/advent_of_code_2018/day_04.ex
@@ -1,5 +1,52 @@
 defmodule AdventOfCode2018.Day04 do
-  def part1(_args) do
+  defmodule State do
+    defstruct guard: nil, asleep_since: nil, guard_sleep_minutes: %{}
+  end
+
+  def part1(args) do
+    {guard, minutes} =
+      args
+      |> String.trim()
+      |> String.split("\n")
+      |> Enum.sort()
+      |> Enum.map(&String.split(&1, ~r/[[\]#: ]+/, trim: true))
+      |> Enum.reduce(%State{}, &parse_line/2)
+      |> Map.get(:guard_sleep_minutes)
+      |> Enum.max_by(fn {guard, minutes} -> length(minutes) end)
+
+    minute =
+      minutes |> Enum.group_by(& &1) |> Map.values() |> Enum.max_by(&length/1) |> List.first()
+
+    guard * minute
+  end
+
+  defp parse_line([_, _, _, "Guard", id, "begins", "shift"], state) do
+    %{state | guard: String.to_integer(id), asleep_since: nil}
+  end
+
+  defp parse_line([_, _, minute, "falls", "asleep"], state) do
+    %{state | asleep_since: String.to_integer(minute)}
+  end
+
+  defp parse_line([_, _, minute, "wakes", "up"], state) do
+    %{
+      state
+      | asleep_since: nil,
+        guard_sleep_minutes: record_sleep(state, String.to_integer(minute))
+    }
+  end
+
+  defp record_sleep(state, minute) do
+    state.guard_sleep_minutes
+    |> Map.update(
+      state.guard,
+      list_minutes(state.asleep_since, minute),
+      fn sleep -> sleep ++ list_minutes(state.asleep_since, minute) end
+    )
+  end
+
+  defp list_minutes(start, finish) do
+    start..(finish - 1) |> Enum.to_list()
   end
 
   def part2(_args) do
--- test/advent_of_code_2018/day_04_test.exs
+++ test/advent_of_code_2018/day_04_test.exs
@@ -3,17 +3,35 @@ defmodule AdventOfCode2018.Day04Test do
 
   import AdventOfCode2018.Day04
 
-  @tag :skip
-  test "part1" do
-    input = nil 
+  test "part1 returns the product of the sleepiest guard's ID and most common sleeping minute" do
+    input = """
+    [1518-11-01 00:05] falls asleep
+    [1518-11-01 00:25] wakes up
+    [1518-11-01 00:00] Guard #10 begins shift
+    [1518-11-01 00:55] wakes up
+    [1518-11-04 00:36] falls asleep
+    [1518-11-02 00:40] falls asleep
+    [1518-11-04 00:02] Guard #99 begins shift
+    [1518-11-03 00:05] Guard #10 begins shift
+    [1518-11-05 00:03] Guard #99 begins shift
+    [1518-11-02 00:50] wakes up
+    [1518-11-03 00:24] falls asleep
+    [1518-11-03 00:29] wakes up
+    [1518-11-01 00:30] falls asleep
+    [1518-11-01 23:58] Guard #99 begins shift
+    [1518-11-05 00:55] wakes up
+    [1518-11-04 00:46] wakes up
+    [1518-11-05 00:45] falls asleep
+    """
+
     result = part1(input)
 
-    assert result
+    assert result == 240
   end
 
   @tag :skip
   test "part2" do
-    input = nil 
+    input = nil
     result = part2(input)
 
     assert result
