9de8e1bcb4dcd140dc08b92f162feb641979dcea
Kerry Buckley <kerryjbuckley@gmail.com>
2018-12-06 21:46:33 +0000

    Day 6 part 1. I think that's my lot.

--- lib/advent_of_code_2018/day_06.ex
+++ lib/advent_of_code_2018/day_06.ex
@@ -1,5 +1,68 @@
 defmodule AdventOfCode2018.Day06 do
-  def part1(_args) do
+  defmodule Grid do
+    defstruct min_x: 0, max_x: 0, min_y: 0, max_y: 0
+  end
+
+  def part1(args) do
+    points =
+      args
+      |> String.split("\n", trim: true)
+      |> Enum.map(&String.split(&1, ", "))
+      |> Enum.map(fn [x, y] -> {String.to_integer(x), String.to_integer(y)} end)
+
+    grid = grid_boundaries(points)
+
+    map =
+      for x <- grid.min_x..grid.max_x, y <- grid.min_y..grid.max_y, into: %{} do
+        {{x, y}, nearest_point(points, {x, y})}
+      end
+      |> Enum.group_by(fn {coords, point} -> point end, fn {coords, point} -> coords end)
+
+    finite_points = finite_points(map, grid)
+
+    {_, points} =
+      map
+      |> Enum.reject(fn {point, _} -> point == :equal end)
+      |> Enum.filter(fn {point, _} -> point in finite_points end)
+      |> Enum.max_by(fn {_, points} -> length(points) end)
+
+    length(points)
+  end
+
+  def grid_boundaries([{x, y} | points]) do
+    points
+    |> Enum.reduce(%Grid{min_x: x, max_x: x, min_y: y, max_y: y}, fn {x, y}, grid ->
+      %{
+        grid
+        | min_x: Enum.min([x, grid.min_x]),
+          max_x: Enum.max([x, grid.max_x]),
+          min_y: Enum.min([y, grid.min_y]),
+          max_y: Enum.max([y, grid.max_y])
+      }
+    end)
+  end
+
+  def nearest_point(points, {x, y}) do
+    distances = points |> Enum.map(fn {x1, y1} = point -> {point, abs(x1 - x) + abs(y1 - y)} end)
+    {_, min} = distances |> Enum.min_by(fn {point, distance} -> distance end)
+
+    distances
+    |> Enum.filter(fn {point, distance} -> distance == min end)
+    |> case do
+      [{point, _}] -> point
+      _ -> :equal
+    end
+  end
+
+  def finite_points(map, grid) do
+    map
+    |> Map.keys()
+    |> Enum.reject(fn point ->
+      map[point]
+      |> Enum.any?(fn {x, y} ->
+        x == grid.min_x || x == grid.max_x || y == grid.min_y || y == grid.max_y
+      end)
+    end)
   end
 
   def part2(_args) do
--- test/advent_of_code_2018/day_06_test.exs
+++ test/advent_of_code_2018/day_06_test.exs
@@ -3,17 +3,63 @@ defmodule AdventOfCode2018.Day06Test do
 
   import AdventOfCode2018.Day06
 
-  @tag :skip
-  test "part1" do
-    input = nil 
+  test "part1 returns the size of the largest finite area" do
+    input = """
+    1, 1
+    1, 6
+    8, 3
+    3, 4
+    5, 5
+    8, 9
+    """
+
     result = part1(input)
 
-    assert result
+    assert result == 17
+  end
+
+  test "grid_boundaries returns the min and max coordinates" do
+    points = [
+      {1, 1},
+      {1, 6},
+      {8, 3},
+      {3, 4},
+      {5, 5},
+      {8, 9}
+    ]
+
+    assert %{min_x: 1, max_x: 8, min_y: 1, max_y: 9} = grid_boundaries(points)
+  end
+
+  describe "nearest_point/2" do
+    test "returns the nearest point by Manhatten distance" do
+      points = [{1, 1}, {1, 6}, {8, 3}]
+      assert nearest_point(points, {2, 5}) == {1, 6}
+    end
+
+    test "returns :equal if there's more than one nearest point" do
+      points = [{1, 1}, {1, 3}, {8, 3}]
+      assert nearest_point(points, {2, 2}) == :equal
+    end
+  end
+
+  test "finite_points returns points whose area doesn't reach an edge of the grid" do
+    grid = %{min_x: 1, max_x: 10, min_y: 1, max_y: 10}
+
+    map = %{
+      {2, 2} => [{1, 3}],
+      {5, 3} => [{5, 1}, {5, 2}],
+      {3, 3} => [{2, 3}],
+      {9, 5} => [{10, 5}],
+      {8, 8} => [{5, 10}]
+    }
+
+    assert finite_points(map, grid) == [{3, 3}]
   end
 
   @tag :skip
   test "part2" do
-    input = nil 
+    input = nil
     result = part2(input)
 
     assert result
